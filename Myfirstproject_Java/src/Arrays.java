9efb47030e273b0995caa4ac0281c8585f8d73e8
juanlp21 <55576203+juanlp21@users.noreply.github.com>
2020-04-14 15:03:42 +0200

    Add files via upload

---
+++ Myfirstproject_Java/src/Arrays.java
@@ -0,0 +1,12 @@
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+		int [] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		System.out.println("La cadena tiene "+ array1.length+ " numeros");
+		for (int cont = 0; cont < (array1.length-1); cont++) {
+			System.out.println(array1[cont]);
+		}
+	}
+
+}
---
+++ Myfirstproject_Java/src/Multiplicador.java
@@ -0,0 +1,14 @@
+
+public class Multiplicador {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cont,numero=5;
+		for(cont=0;cont<=10;cont++){
+			System.out.print (cont);
+			System.out.print (" x ");
+			System.out.print (numero);
+			System.out.print (" = ");
+			System.out.println (cont*numero);
+		}
+	}
+}
